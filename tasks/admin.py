2348d6c1d0f1b32882ac0a2c0cbada30da2676d0
Valerii <valerapiliaev@gmail.com>
2025-09-16 11:27:12 +0300

    feat: register Worker, Position, TaskType and Task in admin panel
    - customize WorkerAdmin With position field
    - add filters and search for Task

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Position, Worker, TaskType, Task
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("position",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name", "position",)}),)
+    list_filter = ("position",)
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "task_type", "priority", "deadline", "is_completed")
+    list_filter = ("task_type", "priority", "is_completed")
+    search_fields = ("name", "description")
+    filter_horizontal = ("assignees",)
