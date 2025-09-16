43faad77ccc7aaf60699e3e56c713b586003dc44
Valerii <valerapiliaev@gmail.com>
2025-09-16 11:01:07 +0300

    feat: add Worker, Position, TaskType and Task models with migrations
    - Worker inherits from AbstractUser and has Position relation
    - Task has TaskType and Worker assignees relations
    - Task uses TextChoices for priority

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+        null=True,
+        blank=True
+    )
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    class PriorityChoices(models.TextChoices):
+        URGENT = "Urgent", "Urgent"
+        HIGH = "High", "High"
+        MEDIUM = "Medium", "Medium"
+        LOW = "Low", "Low"
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=PriorityChoices.choices,
+        default=PriorityChoices.MEDIUM,
+    )
+
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        Worker,
+        related_name="assigned_tasks"
+    )
